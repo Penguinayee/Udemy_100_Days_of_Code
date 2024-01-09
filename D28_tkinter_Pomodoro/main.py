c3a36be9fd2ec1389775ab5a351d3ef13b508b47
Cheng-Yi Chen <penguinayee@gmail.com>
2024-01-09 16:08:29 -0500

    upload D28

--- D28_tkinter_Pomodoro/main.py
+++ D28_tkinter_Pomodoro/main.py
@@ -1,5 +1,6 @@
 
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,27 +10,87 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0 # global variable for replications
+timer = None # hold as global variable first
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
+def reset_timer():
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text = "00:00")
+    check_marks.config(text = "")
+    timer_label.config(text = "Timer", fg = GREEN)
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text = "Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text = "Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text = "Work", fg=GREEN)
+    
+    #check_marks.config(text = "✔" * math.ceil(reps / 2))
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+
+def count_down(count):
+    
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    
+    canvas.itemconfig(timer_text, text = f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer # get the global variable
+        timer = window.after(1000, count_down, count -1) # set the global variable as an after object for after_cancel in the reset_timer function
+    else:
+        start_timer()
+        check_marks.config(text = "✔" * math.ceil(reps / 2))
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodoro")
-window.config(width=100, height=100, bg= YELLOW)
+window.config(padx=100, pady=50, bg= YELLOW)
 
-canvas = Canvas(width=200, height = 200)
-tomato_img = PhotoImage("./D28_tkinter_Pomodoro/tomato.png")
+
+canvas = Canvas(width=200, height = 224, bg = YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file = "./D28_tkinter_Pomodoro/tomato.png")
 canvas.create_image(100, 112, image = tomato_img)
-canvas.create_text(100, 130, text="00:00", fill = "white", font=("Courier",40, "bold"))
-canvas.pack()
+timer_text = canvas.create_text(100, 130, text="00:00", fill = "white", font=(FONT_NAME,40, "bold"))
+canvas.grid(column=1, row = 1)
 
 
+timer_label = Label(text = "Timer", font = (FONT_NAME, 40), fg = GREEN, bg = YELLOW)
+timer_label.grid(column = 1, row = 0)
 
+check_marks = Label(font = (FONT_NAME, 20), fg = GREEN, bg = YELLOW)
+check_marks.grid(column = 1, row = 2)
 
+start_button = Button(text="Start", highlightbackground=YELLOW, command = start_timer)
+start_button.grid(column = 0, row = 2)
+
+reset_button = Button(text="Reset", highlightbackground=YELLOW, command = reset_timer)
+reset_button.grid(column = 2, row = 2)
 
 window.mainloop()
